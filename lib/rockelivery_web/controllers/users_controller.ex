4f660db79e5fd86722a507f0851f375428faa816
Wesley Silva <wesleyysilva@yahoo.com.br>
2022-12-03 20:45:19 -0300

    create and configure FallbackController

--- lib/rockelivery_web/controllers/users_controller.ex
+++ lib/rockelivery_web/controllers/users_controller.ex
@@ -1,7 +1,10 @@
 defmodule RockeliveryWeb.UsersController do
   use RockeliveryWeb, :controller
 
+  alias RockeliveryWeb.FallbackController
   alias Rockelivery.User
+
+  action_fallback FallbackController
 
   def create(conn, params) do
     with {:ok, %User{} = user} <- Rockelivery.create_user(params) do
